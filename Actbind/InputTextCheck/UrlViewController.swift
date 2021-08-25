c153380b85cba54649b728017ef1b99c00a8f6f5
Kaito Kakimoto <kakimotokaito0730@icloud.com>
2021-08-26 06:31:32 +0900

    ♻️ update : URLの正規表現の調整

--- Actbind/InputTextCheck/UrlViewController.swift
+++ Actbind/InputTextCheck/UrlViewController.swift
@@ -14,7 +14,7 @@ final class UrlViewController: UIViewController {
     
     // URLかどうかのクラス
     final class func isValidUrl(_ string: String) -> Bool {
-        let urlRegEx = "^(http://|https://)[A-Za-z0-9.-]+(?!.*\\|\\w*$)"
+        let urlRegEx = "^(http|https)\\://([a-zA-Z0-9\\.\\-]+(\\:[a-zA-Z0-9\\.&amp;%\\$\\-]+)*@)*((25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9])\\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[0-9])|localhost|([a-zA-Z0-9\\-]+\\.)*[a-zA-Z0-9\\-]+\\.(com|edu|gov|int|mil|net|org|biz|arpa|info|name|pro|aero|coop|museum|[a-zA-Z]{2}))(\\:[0-9]+)*(/($|[a-zA-Z0-9\\.\\,\\?\\'\\\\\\+&amp;%\\$#\\=~_\\-]+))*$"
         let urlTest = NSPredicate(format: "SELF MATCHES %@", urlRegEx)
         let result = urlTest.evaluate(with: string)
         return result
