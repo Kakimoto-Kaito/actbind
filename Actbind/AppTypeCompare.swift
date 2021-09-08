3b6ae254893c9915dcdcd9a738adfa6045439a2d
Kakimoto-Kaito <kakimotokaito0730@icloud.com>
2021-09-08 22:20:48 +0900

    ♻️ update : Version取得をitunes APIから自作APIに変更

--- Actbind/AppTypeCompare.swift
+++ Actbind/AppTypeCompare.swift
@@ -15,32 +15,43 @@ enum AppTypeCompareType {
 
 enum AppTypeCompare {
     static func toAppStoreVersion(completion: @escaping (AppTypeCompareType) -> Void) {
-        let url = URL(string: "https://itunes.apple.com/lookup?id=1579280491")!
-        let request = URLRequest(url: url)
-
+        let api = KeyManager().getValue(key: "API") as? String
+        
+        let url = URL(string: api! + "appversion")!
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST" // Postリクエストを送る(このコードがないとGetリクエストになる)
+        request.addValue("application/json", forHTTPHeaderField: "content-type")
+        // request.addValue("", forHTTPHeaderField: "authorization") //APIKey
+        
         let task = URLSession.shared.dataTask(with: request) { data, response, error in
             // ここのエラーはクライアントサイドのエラー(ホストに接続できないなど)
-            if error != nil {
-                completion(.error)
-                return
+            if let error = error {
+                DispatchQueue.main.async {
+                    print("クライアントエラー: \(error.localizedDescription)")
+                    completion(.error)
+                }
             }
-
+            
             guard let data = data, let response = response as? HTTPURLResponse else {
-                print("no data or no response")
+                completion(.error)
                 return
             }
             
             if response.statusCode == 200 {
+                let json = String(data: data, encoding: .utf8)!
+                print(json)
+                
                 do {
-                    let jsonData = try JSONSerialization.jsonObject(with: data) as? [String: Any]
-                    guard let storeVersion = ((jsonData?["results"] as? [Any])?.first as? [String: Any])?["version"] as? String,
-                          let appVersion = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String
-                    else {
-                        completion(.error)
-                        return
-                    }
+                    let versionNumberArray = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.allowFragments) as! [Any]
+                    print(versionNumberArray)
                     
-                    switch storeVersion.compare(appVersion, options: .numeric) {
+                    let versionNumber = versionNumberArray.map { versionNumber -> [String: Any] in
+                        versionNumber as! [String: Any]
+                    }
+                    let storeVersion = versionNumber[0]["version_number"] as! String
+                    let appVersion = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String
+                    
+                    switch storeVersion.compare(appVersion!, options: .numeric) {
                     case .orderedAscending:
                         // appVersion > storeVersion
                         completion(.beta)
